a19507a72317f0306692c5a95bc2a95c77b3a05e
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2022-03-07 14:59:55 +0000

    Get water

--- .ccls-cache/@home@runner@Xcamp-202c/main.cpp
+++ .ccls-cache/@home@runner@Xcamp-202c/main.cpp
@@ -1,36 +1,8 @@
 #include <iostream>
-#include <set>
 #include <vector>
+#include <algorithm>
+#include <set>
+#include <stdio.h>
 using namespace std;
-int n, l, p;
-vector<vector<int>> binary;
-int times = 0;
-void dfs(vector<int> input, int pos, int one) {
-  if (pos == l) {
-    binary.push_back(input);
-    times++;
-    if (times == p) {
-      for (int i = 0; i < l; i++) {
-         cout << input[i];
-      }
-       cout << "\n";
-      exit(0);
-    }
-    return;
-  }
-
-  input[pos] = 0;
-  dfs(input, pos + 1, one);
-  if (one < n) {
-    input[pos] = 1;
-    dfs(input, pos + 1, one + 1);
-  }
-}
 int main() {
-  cin >> l >> n >> p;
-  vector<int> arr;
-  for (int i = 0; i < l; i++) {
-    arr.push_back(0);
-  }
-  dfs(arr, 0, 0);
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 #include <set>
+#include <stdio.h>
 using namespace std;
 int main() {
 }
