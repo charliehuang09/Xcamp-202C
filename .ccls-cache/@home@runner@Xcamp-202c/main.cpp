2dc5e2e786a605b5a47d55022612a5da44380997
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2022-03-10 19:37:30 +0000

    fix include

--- .ccls-cache/@home@runner@Xcamp-202c/main.cpp
+++ .ccls-cache/@home@runner@Xcamp-202c/main.cpp
@@ -2,8 +2,30 @@
 #include <vector>
 #include <algorithm>
 #include <set>
-#include <stdio.h>
 #include <stack>
+#include <map>
+#include <stdio.h>
 using namespace std;
 int main() {
+    int length, answers;
+    cin >> length >> answers;
+    map<int, int> input;
+    cout << input[213123];
+    return 0;
+    int t;
+    for (int i = 0; i < length; i++){
+        cin >> t;
+        input[t] = i + 1;
+    }
+    for (int i = 0; i < answers; i++){
+        cin >> t;
+        if (input[t] == 0){
+            cout << length + 1;
+        }
+        else{
+            cout << input[t];
+        }
+        cout << "\n";
+    }
+
 }
