6955c00235e21524dd8d70ebe399e99c8eaf77c8
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2022-03-12 19:12:47 +0000

    reset main and include stack

--- .ccls-cache/@home@runner@Xcamp-202c/main.cpp
+++ .ccls-cache/@home@runner@Xcamp-202c/main.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -6,47 +5,7 @@
 #include <stack>
 #include <map>
 #include <stdio.h>
+#include <stack>
 using namespace std;
-int largest (stack <int> stak){
-    int largest = 0;
-    while (!stak.empty()){
-        if (largest < stak.top()){
-            largest = stak.top();
-        }
-        stak.pop();
-    }
-    return largest;
-}
 int main() {
-  int n;
-    cin >> n;
-    stack <int> stack;
-    int a;
-    for(int i = 0; i < n; i++){
-        cin>>a;
-        if(a == 0){
-            cin >> a;
-            stack.push(a);
-        }else if(a == 1 && stack.size() > 0){
-            stack.pop();
-        }
-        else if (a == 2){
-            cout << largest(stack) << "\n";
-        }
-    }
-
 }
-// 33
-// 0 1
-// 0 2
-// 2
-// 0 4
-// 0 2
-// 2
-// 1
-// 2
-// 1
-// 1
-// 2
-// 1
-// 2
