6af1a015ff2042f5b527176c3978db299e3a292b
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2022-04-26 17:47:20 +0000

    Number of connected blocks

--- .ccls-cache/@home@runner@Xcamp-202c/main.cpp
+++ .ccls-cache/@home@runner@Xcamp-202c/main.cpp
@@ -1,70 +1,44 @@
-#include <iostream>
-#include <vector>
 #include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
 #include <set>
 #include <stack>
-#include <map>
 #include <stdio.h>
-#include <stack>
 #include <stdlib.h>
-#include <queue>
-#include <bitset>
+#include <vector>
 using namespace std;
 
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
-int col, row;
-int output = 0;
+vector<bool> visited;
+int main() {
+  int nodes, edges;
+  cin >> nodes >> edges;
 
-bool inbound(int x, int y){
-    if (x < 0){
-        return false;
-    }
-    if (y < 0){
-        return false;
-    }
-    if (x >= col){
-        return false;
-    }
-    if (y >= row){
-        return false;
-    }
-    return true;
-}
+  vector<int> adj[nodes];
+  int node1, node2;
 
-void dfs (vector<bitset<7>> maze, int x, int y){
-    if (x == col - 1 && y == 0){//changed
-        output++;
-        return;
-    }
-    maze[x][y].flip();
-    for (int i = 0; i < 4; i++){
-        if (inbound(x + dx[i], y + dy[i])){
-            if (maze[x + dx[i]][y + dy[i]]){
-                dfs(maze, x + dx[i], y + dy[i]);
-            }
+  for (int i = 0; i < edges; i++) {
+    cin >> node1 >> node2;
+    adj[node1].push_back(node2);
+    adj[node2].push_back(node1);
+
+    visited.push_back(false);
+  }
+
+  for (int i = 0; i < edges; i++) {
+    if (!visited[i]){
+      stack<vector<int>> s;
+      while (!s.empty()) {
+        vector<int> pt = s.top();
+        s.pop();
+  
+        for (int i = 0; i < s.size(); i++) {
+          if (!visited[pt[i]]) {
+            visited[pt[i]] = true;
+          }
         }
+      }
     }
+  }
 }
-int main(){
-    cin >> col >> row;
-    vector<bitset<7>> maze;
-
-    string t;
-    for (int i = 0; i < col; i++){
-        cin >> t;
-        maze.push_back(bitset<7>(t, 0, t.size(), '#', '*'));
-    }
-
-    dfs(maze, 0,row - 1);// changed
-
-    cout << output;
-
-    return 0;
-}
-//5 6
-//*****#
-//*###*#
-//*###*#
-//*###*#
-//******
--- main.cpp
+++ main.cpp
@@ -6,65 +6,9 @@
 #include <map>
 #include <stdio.h>
 #include <stack>
-#include <stdlib.h>
+#include <stdlib.h> 
 #include <queue>
 #include <bitset>
 using namespace std;
-
-int dx[4] = {0,0,-1,1};
-int dy[4] = {1,-1,0,0};
-int col, row;
-int output = 0;
-
-bool inbound(int x, int y){
-    if (x < 0){
-        return false;
-    }
-    if (y < 0){
-        return false;
-    }
-    if (x >= col){
-        return false;
-    }
-    if (y >= row){
-        return false;
-    }
-    return true;
+int main() {
 }
-
-void dfs (vector<bitset<7>> maze, int x, int y){
-    if (x == col - 1 && y == 0){//changed
-        output++;
-        return;
-    }
-    maze[x][y].flip();
-    for (int i = 0; i < 4; i++){
-        if (inbound(x + dx[i], y + dy[i])){
-            if (maze[x + dx[i]][y + dy[i]]){
-                dfs(maze, x + dx[i], y + dy[i]);
-            }
-        }
-    }
-}
-int main(){
-    cin >> col >> row;
-    vector<bitset<7>> maze;
-
-    string t;
-    for (int i = 0; i < col; i++){
-        cin >> t;
-        maze.push_back(bitset<7>(t, 0, t.size(), '#', '*'));
-    }
-
-    dfs(maze, 0,row - 1);// changed
-
-    cout << output;
-
-    return 0;
-}
-//5 6
-//*****#
-//*###*#
-//*###*#
-//*###*#
-//******
