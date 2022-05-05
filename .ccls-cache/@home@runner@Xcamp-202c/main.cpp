80830d92dda7afd9a583b751d831119afc937f7c
Charlie Huang <41605449+charliehuang09@users.noreply.github.com>
2022-05-05 03:01:56 +0000

    topological sequence

--- .ccls-cache/@home@runner@Xcamp-202c/main.cpp
+++ .ccls-cache/@home@runner@Xcamp-202c/main.cpp
@@ -1,44 +1,14 @@
-#include <algorithm>
-#include <bitset>
 #include <iostream>
-#include <map>
-#include <queue>
+#include <vector>
+#include <algorithm>
 #include <set>
 #include <stack>
+#include <map>
 #include <stdio.h>
-#include <stdlib.h>
-#include <vector>
+#include <stack>
+#include <stdlib.h> 
+#include <queue>
+#include <bitset>
 using namespace std;
-
-vector<bool> visited;
 int main() {
-  int nodes, edges;
-  cin >> nodes >> edges;
-
-  vector<int> adj[nodes];
-  int node1, node2;
-
-  for (int i = 0; i < edges; i++) {
-    cin >> node1 >> node2;
-    adj[node1].push_back(node2);
-    adj[node2].push_back(node1);
-
-    visited.push_back(false);
-  }
-
-  for (int i = 0; i < edges; i++) {
-    if (!visited[i]){
-      stack<vector<int>> s;
-      while (!s.empty()) {
-        vector<int> pt = s.top();
-        s.pop();
-  
-        for (int i = 0; i < s.size(); i++) {
-          if (!visited[pt[i]]) {
-            visited[pt[i]] = true;
-          }
-        }
-      }
-    }
-  }
 }
